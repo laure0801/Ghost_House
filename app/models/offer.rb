18a5cbf0584124878c9395255e08a98348eedb7e
laure reina <laure.reina@gmail.com>
2021-12-15 16:13:53 +0100

    status dans reservation model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,9 +1,9 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
 
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :price, presence: true 
+  validates :price, presence: true
 end

8c3722e8ccf24065c617492a420359a60f9d555e
laure reina <laure.reina@gmail.com>
2021-12-16 17:53:15 +0100

    méthod accept/decline et profile ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,14 @@ class PagesController < ApplicationController
   def home
     @offers = Offer.all
   end
+
+  def profile
+    @user = current_user
+    @reservation_requests = @user.reservations
+    @reservation_demands = []
+    @reservations = Reservation.all
+    @reservations.each do |reservation|
+      @reservation_demands << reservation if reservation.offer.user == @user
+    end
+  end
 end
